560fd7204f9c9814e7f3b39677b7d7715b1acc1b
krasnovss91 <krasnovstanislav91@gmail.com>
2021-11-17 22:15:37 +0400

    teachers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,33 @@ package com.company;
 import com.company.entuty.Student;
 import com.company.entuty.Teacher;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
     }
-//4 метод генерации строк
+
+
+    private static final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String generateRandom(int length) { //метод генерации строк
+        Random random = new SecureRandom();
+        if (length <= 0) {
+            throw new IllegalArgumentException("String length must be a positive integer");
+        }
+
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return sb.toString();
+    }
 
     public List<Student> generateStudentsOneCourse(){//заполнять через соответствующий конструктор
 
@@ -31,6 +49,9 @@ public class Main {
 
         List<Teacher> teachers = new ArrayList<Teacher>();
 
+        teachers.add(new Teacher(generateRandom(5),generateRandom(5),generateRandom(7),generateRandom(7),generateRandom(7),generateRandom(8),true,generateRandom(8)));
+        teachers.add(new Teacher(generateRandom(5),generateRandom(5),generateRandom(5),generateRandom(6),generateRandom(9),generateRandom(8),false,generateRandom(6)));
+        teachers.add(new Teacher(generateRandom(5),generateRandom(5),generateRandom(8),generateRandom(8),generateRandom(10),generateRandom(6),true,generateRandom(8)));
 
         return teachers;
     }
